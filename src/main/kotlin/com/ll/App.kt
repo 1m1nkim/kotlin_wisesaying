40bff09ce409ef7c08d53712e6ddacc01f29697a
1m1nkim <window12ty@naver.com>
2025-02-15 14:40:35 +0900

    Feat: Rq(request)에 input 명령어 파싱

--- src/main/kotlin/com/ll/App.kt
+++ src/main/kotlin/com/ll/App.kt
@@ -12,9 +12,12 @@ class App {
             // !!는 변수가 null을 리턴할리가 없으니까 ?를 굳이 사용하지 않아도 된다는 의미
             // 확실한 경우에만 사용하는게 좋음, 남발 x
 
-            if( input =="종료")
+            val rq = Rq(input)
+                //rq는 리퀘스트
+
+            if( rq.action =="종료")
                 break
-            else if (input =="등록") {
+            else if (rq.action =="등록") {
                 print("명언: ")
                 val content = readlnOrNull()!!.trim()
                 print("작가: ")
@@ -25,7 +28,7 @@ class App {
                 wiseSayings.add(WiseSaying(id, content, author))
 
                 println("${id}번 명언이 등록되었습니다.")
-            }else if(input == "목록"){
+            }else if(rq.action == "목록"){
                 if(wiseSayings.isEmpty()){
                     println("명언이 없습니다.")
                     continue
@@ -35,6 +38,15 @@ class App {
                 wiseSayings.forEach{
                     println("${it.id} / ${it.content} / ${it.author}")
                 }
+            }else if(rq.action == "삭제"){
+                val id = rq.getParamValueAsInt("id", 0)
+
+                if(id==0){
+                    println("id를 정확히 입력해주세요")
+                    continue
+                }
+
+                println("입력된 id: $id")
             }
         }
     }
