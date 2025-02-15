4c8418c138bb65cf7b2046f8f5f14bd9896e134c
1m1nkim <window12ty@naver.com>
2025-02-15 14:14:25 +0900

    Feat: wiseSayings list 추출하기

--- src/main/kotlin/com/ll/App.kt
+++ src/main/kotlin/com/ll/App.kt
@@ -1,9 +1,10 @@
-package com.ll
+package com.ll.com.ll
 
 class App {
     fun run(){
         println("== 명언 앱 ==")
-        var lastId = 0;
+        var lastId = 0
+        val wiseSayings = mutableListOf<WiseSaying>()
         while (true){
             print("명령어: ")
             val input = readlnOrNull()!!.trim()     //입력을 받거나 null일수도 있다.
@@ -13,7 +14,7 @@ class App {
 
             if( input =="종료")
                 break
-            else if (input =="등록"){
+            else if (input =="등록") {
                 print("명언: ")
                 val content = readlnOrNull()!!.trim()
                 print("작가: ")
@@ -21,7 +22,19 @@ class App {
 
                 val id = ++lastId
 
+                wiseSayings.add(WiseSaying(id, content, author))
+
                 println("${id}번 명언이 등록되었습니다.")
+            }else if(input == "목록"){
+                if(wiseSayings.isEmpty()){
+                    println("명언이 없습니다.")
+                    continue
+                }
+                println("번호 / 작가 / 명언")
+                println("---------------------")
+                wiseSayings.forEach{
+                    println("${it.id} / ${it.content} / ${it.author}")
+                }
             }
         }
     }
---
+++ src/main/kotlin/com/ll/WiseSaying.kt
@@ -0,0 +1,9 @@
+package com.ll.com.ll
+
+class WiseSaying(
+    val id: Int,
+    var content: String,
+    var author: String
+) {
+
+}
